223bc65893fa159d8faede902ff93ff341aa5a1d
Zahra <zahra.kirschblute@hotmail.com>
2021-02-18 21:02:32 +0000

    Create json containing info regarding projectlist

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ function Projects() {
  
 <div className="container pr-5 pl-5 pt-3 mx-auto">
 <h3 class="mb-3 fw-light display-4">PROJECTS</h3>
-  <Project title = {projectlist.socialmediaapp.Title} description = {projectlist.socialmediaapp.description} order={projectlist.socialmediaapp.order} ></Project>
+  <Project title = {projectlist.schnack.title} description = {projectlist.schnack.description} order={projectlist.schnack.order} ></Project>
 </div>
   );
 }
