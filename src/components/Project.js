3d50705ac96923f628ac3115eb70112396b1ddbc
Zahra <zahra.kirschblute@hotmail.com>
2021-02-18 21:55:47 +0000

    Update json file with img links

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,24 @@
 import React from "react";
-import wejournal from "../assets/wejournal.PNG"
 import {FiGithub, FiEye} from "react-icons/fi"
+import { contains } from "jquery";
 
 function Project(props) {
 
   return (
 
 
-  <div className="row">
+  <div className="row mb-5">
   <div className={`col-6 order-${props.order} `}>
       <h3>{props.title}</h3>
     <p>{props.description}</p>
 <div className="row p-3">
-
 <a className="a-custom col-6"><FiGithub className="svg-size"></FiGithub></a>
 
 <a className="a-custom col-6"><FiEye className="svg-size"></FiEye></a>
 </div>
 </div>
     <div class="col-6">
-<img src={wejournal} className="project-img-size"/>
+<div style={{height: 290, width: 520, backgroundSize: 'cover', backgroundImage: `url("${props.image}")`}}></div>
 </div>
 
 </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,8 +8,8 @@ function Projects() {
  
 <div className="container pr-5 pl-5 pt-3 mx-auto">
 <h3 class="mb-3 fw-light display-4">PROJECTS</h3>
-  <Project title = {projectlist.schnack.title} description = {projectlist.schnack.description} order={projectlist.schnack.order} ></Project>
-  <Project title = {projectlist.wejournal.title} description = {projectlist.wejournal.description} order={projectlist.wejournal.order} ></Project>
+  <Project title = {projectlist.schnack.title} description = {projectlist.schnack.description} order={projectlist.schnack.order} image={projectlist.schnack.image} ></Project>
+  <Project title = {projectlist.wejournal.title} description = {projectlist.wejournal.description} order={projectlist.wejournal.order} image={projectlist.wejournal.image} ></Project>
 </div>
   );
 }
